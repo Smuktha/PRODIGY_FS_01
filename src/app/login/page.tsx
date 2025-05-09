54da096b31d1b23a5190b78adb46a25d0a44a80e
Muktha <115140893+Smuktha@users.noreply.github.com>
2025-05-09 12:05:26 +0530

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,10 +5,6 @@ import {useRouter} from "next/navigation";
 import axios from "axios";
 import { toast } from "react-hot-toast";
 
-
-
-
-
 export default function LoginPage() {
     const router = useRouter();
     const [user, setUser] = React.useState({
@@ -60,7 +56,7 @@ export default function LoginPage() {
         <label htmlFor="password">password</label>
         <input 
         className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
-            id="password"
+            id="password"              
             type="password"
             value={user.password}
             onChange={(e) => setUser({...user, password: e.target.value})}
